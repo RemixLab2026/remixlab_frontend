a946cc367b73fe16c3b7a6c545cc53b55234685b
ohoCoding <youngho7482@gmail.com>
2026-03-23 21:46:12 +0900

    fix: :bug: use accessToken from auth store in axios interceptor

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,22 +1,21 @@
-import axios, {type InternalAxiosRequestConfig } from 'axios';
+import axios, { type InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
-const BASE_URL = import.meta.env.VITE_API_URL || '/api-proxy';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api-proxy';
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-// config 매개변수에 InternalAxiosRequestConfig 타입 지정
 axiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    const token = useAuthStore.getState().accessToken;
+  const token = useAuthStore.getState().accessToken;
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });

7901b134c66c7d7e718e3eaf5f83f92186c70e43
ohofront <youngho7482@gmail.com>
2026-03-23 13:43:08 +0900

    Update ProtectedRoute to check auth using accessToken instead of isLogin

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuthStore } from '@/store/authStore';
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const isLogin = useAuthStore((state) => state.isLogin);
+  const isLogin = useAuthStore((state) => !!state.accessToken);
 
   if (!isLogin) {
     return <Navigate to='/login' replace />;
